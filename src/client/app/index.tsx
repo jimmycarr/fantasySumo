f0a0b59a254551f1dd32641e8cfe662fc5e43a2d
James Carr <james.carr@uk.ibm.com>
2019-02-22 12:09:01 +0700

    Added font loader and titillium font

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import WebFont from "webfontloader";
 import App from "./components/App";
+
+WebFont.load({
+  google: {
+    families: ["Titillium Web:300,400,700", "sans-serif"]
+  }
+});
 
 render(<App />, document.getElementById("root"));
