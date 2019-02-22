0c333a344e3a3114501db8d9a5e1a700bf34a889
James Carr <james.carr@uk.ibm.com>
2019-02-22 12:14:06 +0700

    Updated header bar styles

--- src/client/app/components/HeaderBar.tsx
+++ src/client/app/components/HeaderBar.tsx
@@ -3,13 +3,20 @@ import styled from "styled-components";
 
 class HeaderBar extends React.Component {
   public render() {
-    return <FullSpanBar>Welcome to Fantasy Sumo</FullSpanBar>;
+    return (
+      <FullSpanBar>
+        <div>Welcome to Fantasy Sumo! - ファンタジー相撲</div>
+      </FullSpanBar>
+    );
   }
 }
 
 const FullSpanBar = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: 40px;
+  padding: 0 8px;
   background-color: black;
   color: white;
   z-index: 100;
